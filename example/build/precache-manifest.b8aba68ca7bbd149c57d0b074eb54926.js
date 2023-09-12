9f9657677d757ef86cb5ef51df55d705d6d49dac
dgarcia <dgarcia@grupoferretti.com>
2023-09-12 10:27:07 -0500

    master: cambios

---
+++ example/build/precache-manifest.b8aba68ca7bbd149c57d0b074eb54926.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2b3a3c82febf84d9ff3d8497e5f5538",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cead3862919155cdc985",
+    "url": "./static/css/2.ee222718.chunk.css"
+  },
+  {
+    "revision": "dfd60c4bc20d33be905c",
+    "url": "./static/css/main.d34f1f76.chunk.css"
+  },
+  {
+    "revision": "cead3862919155cdc985",
+    "url": "./static/js/2.b7dc0cc5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.b7dc0cc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfd60c4bc20d33be905c",
+    "url": "./static/js/main.ffcb1488.chunk.js"
+  },
+  {
+    "revision": "44ec3b6ad531ba680e03",
+    "url": "./static/js/runtime-main.fade9933.js"
+  },
+  {
+    "revision": "26413dab4d79dfe30dbcf52f9b596340",
+    "url": "./static/media/gf.26413dab.svg"
+  },
+  {
+    "revision": "b099b528cc9c7d3e25bbfc3e7c6b8983",
+    "url": "./static/media/gf.b099b528.woff"
+  },
+  {
+    "revision": "c4d62ca02815a976b4ed5142b5232b65",
+    "url": "./static/media/gf.c4d62ca0.eot"
+  },
+  {
+    "revision": "de7e80b516635815b1e0458febf9fa27",
+    "url": "./static/media/gf.de7e80b5.ttf"
+  }
+]);
